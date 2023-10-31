212220c1363d45e5d6abed6d87dafc6c0b95b5e7
mohamed mokhtar <126124106+mohamedmokhtaarr@users.noreply.github.com>
2023-10-31 16:40:15 +0200

    Add Seed Folder

---
+++ HRMS/HRMS.Application/Context/SeedContext.cs
@@ -0,0 +1,21 @@
+﻿using HRMS.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Application.Context
+{
+    public class SeedContext
+    {
+        private readonly DBContext context;
+
+        public SeedContext(DBContext context)
+        {
+            this.context = context;
+        }
+
+
+    }
+}
