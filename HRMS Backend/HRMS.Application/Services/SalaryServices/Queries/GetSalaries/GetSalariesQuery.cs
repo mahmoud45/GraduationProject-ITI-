c032c7733bc13ce4f91e37eeb16909bc5887d5c0
mohamed mokhtar <126124106+mohamedmokhtaarr@users.noreply.github.com>
2023-11-04 04:03:55 +0200

    Finish Salary Page

--- HRMS Backend/HRMS.Application/Services/SalaryServices/Queries/GetSalaries/GetSalariesQuery.cs
+++ HRMS Backend/HRMS.Application/Services/SalaryServices/Queries/GetSalaries/GetSalariesQuery.cs
@@ -3,14 +3,14 @@ using MediatR;
 
 namespace HRMS.Application.Services.SalaryServices.Queries.GetSalaries
 {
-    public class GetSalariesQuery: IRequest<List<SalaryDTO>>
+    public class GetSalariesQuery: IRequest<PaginatedDtO>
     {
 
         int pageNumber = 1;
 
         public int PageNumber { get { return pageNumber; } set { if (value > 1) pageNumber = value; } }
 
-        int pageSize = 9;
+        int pageSize = 9;   
         public int PageSize { get { return pageSize; } set { if (value > 1) pageSize = value; } }
         public string Search { get; set; } = "";
         public string Month { get; set; } = "";
--- HRMS Backend/HRMS.Application/Services/SalaryServices/Queries/GetSalaries/GetSalariesQueryHandler.cs
+++ HRMS Backend/HRMS.Application/Services/SalaryServices/Queries/GetSalaries/GetSalariesQueryHandler.cs
@@ -2,10 +2,11 @@
 using HRMS.Application.Repository.SalaryRepository;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HRMS.Application.Services.SalaryServices.Queries.GetSalaries
 {
-    public class GetSalariesQueryHandler : IRequestHandler<GetSalariesQuery, List<SalaryDTO>>
+    public class GetSalariesQueryHandler : IRequestHandler<GetSalariesQuery, PaginatedDtO>
     {
         ISalaryRepository _salaryRepository;
         private readonly IMapper _mapper;
@@ -15,16 +16,16 @@ namespace HRMS.Application.Services.SalaryServices.Queries.GetSalaries
             this._salaryRepository = salaryRepository;
             this._mapper = mapper;
         }
-        public async Task<List<SalaryDTO>> Handle(GetSalariesQuery request, CancellationToken cancellationToken)
+        public async Task<PaginatedDtO> Handle(GetSalariesQuery request, CancellationToken cancellationToken)
         {
             try
             {
-                if (request.Month == null)
+                if (string.IsNullOrEmpty(request.Month))
                 {
                     request.Month = (DateTime.Now.Month - 1).ToString();
                 }
 
-                if (request.Year == null)
+                if (string.IsNullOrEmpty(request.Year))
                 {
                     request.Year = DateTime.Now.Year.ToString();
                 }
@@ -37,14 +38,12 @@ namespace HRMS.Application.Services.SalaryServices.Queries.GetSalaries
                     .Include(x => x.Department)
                     .Include(x => x.SpecialSettings)
                     .Include(x => x.Attendance.Where(a => a.ArrivalTime != null && a.ArrivalTime.Month.ToString() == request.Month && a.ArrivalTime.Year.ToString() == request.Year))
-                    .Skip((request.PageNumber - 1) * (request.PageSize))
-                    .Take(request.PageSize).ToList();
+                    .ToList();
 
+                var count = empSalary.Count();
 
-
-
-
-                var mappedObjs = _mapper.Map<List<SalaryDTO>>(empSalary);
+                var mappedObjs = _mapper.Map<List<SalaryDTO>>(empSalary.Skip((request.PageNumber - 1) * (request.PageSize))
+                    .Take(request.PageSize));
                 int bounsHours = 0;
                 int penalityHours = 0;
                 for (int i = 0; i < mappedObjs.Count; i++)
@@ -62,16 +61,17 @@ namespace HRMS.Application.Services.SalaryServices.Queries.GetSalaries
                         }
                     }
                     penalityHours += mappedObjs[i].Absence * 8;
-                    
+
                     mappedObjs[i].TotalBouns += bounsHours * mappedObjs[i].SpecialSettingsBonus;
                     mappedObjs[i].TotalPenality += penalityHours * mappedObjs[i].SpecialSettingsPenality;
                     mappedObjs[i].TotalSalary += (mappedObjs[i].Salary + mappedObjs[i].TotalBouns) - mappedObjs[i].TotalPenality;
-
+                    mappedObjs[i].SpecialSettingsBonus = bounsHours;
+                    mappedObjs[i].SpecialSettingsPenality= penalityHours;
                     bounsHours = 0;
                     penalityHours = 0;
                 }
 
-                return mappedObjs;
+                return new PaginatedDtO() { salaryDTOs = mappedObjs, pageCount = count % request.PageSize == 0 ? count/request.PageSize: (count / request.PageSize) + 1};
 
             }
             catch (Exception ex)
